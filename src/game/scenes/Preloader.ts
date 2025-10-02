c006f580716777ab36e2ef58a52d2f7f1e772460
Phuong Dinh <phucphuongnhanh3@gmail.com>
2025-10-02 13:29:44 +0700

    commit_4

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -15,10 +15,10 @@ export default class Preloader extends Scene {
 
     // Load tài nguyên cho game
     this.load.image('background', '../assets/bg_1.png'); 
-    this.load.image('arrow', '../assets/arrow_1.png'); 
+    this.load.image('arrow', '../assets/arrow_4.png'); 
     this.load.image('ground', '../assets/ground.png'); 
     this.load.image('leloi', '../assets/leloi_1.png'); // nhân vật chính
-    this.load.image('enemy', 'assets/minh_soldier.png'); // lính Minh
+    this.load.image('enemy', 'assets/enemy.png'); // lính Minh
     this.load.image('sword', 'assets/sword.png'); // gươm thần
   }
 
