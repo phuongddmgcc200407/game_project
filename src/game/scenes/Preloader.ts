3e5117366bdaec4bc55bdcacfad0facf113a7e25
Phuong Dinh <phucphuongnhanh3@gmail.com>
2025-10-04 15:00:25 +0700

    commit_5

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -14,12 +14,27 @@ export default class Preloader extends Scene {
     }).setOrigin(0.5);
 
     // Load tài nguyên cho game
-    this.load.image('background', '../assets/bg_1.png'); 
-    this.load.image('arrow', '../assets/arrow_4.png'); 
-    this.load.image('ground', '../assets/ground.png'); 
-    this.load.image('leloi', '../assets/leloi_1.png'); // nhân vật chính
-    this.load.image('enemy', 'assets/enemy.png'); // lính Minh
+    this.load.image('background', '../assets/bg_1.png');
+    this.load.image('arrow', '../assets/arrow_4.png');
+    this.load.image('ground', '../assets/ground.png');
     this.load.image('sword', 'assets/sword.png'); // gươm thần
+    // nhân vật chính 1
+    this.load.image('leloi1', '../assets/leloi_1.png'); 
+    this.load.image('leloi2', '../assets/leloi_2.png'); 
+    this.load.image('leloi3', '../assets/leloi_3.png'); 
+    this.load.image('leloi4', '../assets/leloi_4.png'); 
+    this.load.image('leloi5', '../assets/leloi_5.png'); 
+    this.load.image('leloi6', '../assets/leloi_6.png'); 
+
+    //enemy minh_1
+    this.load.image('enemy1', 'assets/m1_1.png');
+    this.load.image('enemy2', 'assets/m1_2.png');
+    this.load.image('enemy3', 'assets/m1_3.png');
+    this.load.image('enemy3_2', 'assets/m1_3_2.png');
+    this.load.image('enemy3_3', 'assets/m1_3_3.png');
+    this.load.image('enemy3_4', 'assets/m1_3_4.png');
+    this.load.image('enemy4', 'assets/m1_4.png');
+
   }
 
   create(): void {
