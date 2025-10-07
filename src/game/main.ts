0277572989124e28d94220e88d44f24ceacaf227
Phuong Dinh <phucphuongnhanh3@gmail.com>
2025-10-07 16:05:13 +0700

    commit_9

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,29 +1,30 @@
 import Phaser from 'phaser';
-// Import các scene
 import Boot from './scenes/Boot';
 import Preloader from './scenes/Preloader';
 import MainMenu from './scenes/MainMenu';
 import GameScene from './scenes/Game';
 // import GameOver from './scenes/GameOver';
-// ✨ THÊM: Import LobbyScene
 import LobbyScene from './scenes/Lobby';
 
 export default function StartGame(parentId: string) {
-    const config: Phaser.Types.Core.GameConfig = {
-        type: Phaser.AUTO,
+  const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    parent: parentId,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: {x:100, y: 600 },
+        debug: false,
+      },
+    },
+    scale: {
+        mode: Phaser.Scale.ENVELOP,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1900,
         height: 1000,
-        parent: parentId, 
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: {x:100, y: 600 }, 
-                debug: false          
-            }
-        }
-        ,
-        scene: [Boot, Preloader, MainMenu, LobbyScene, GameScene],
-    };
+      },
+    scene: [Boot, Preloader, MainMenu, LobbyScene, GameScene],
+  };
 
-    return new Phaser.Game(config);
+  return new Phaser.Game(config);
 }
